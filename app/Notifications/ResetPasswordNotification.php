207a1113cf67710a97a96c5648d97b49b465b000
Luciana Sato <sato.lu@gmail.com>
2018-03-26 21:12:15 -0300

    Adds reset password customization

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends ResetPassword
+{
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Você está recebendo este email porque uma notificação de recuperação de senha da sua conta nos foi enviada.')
+            ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
