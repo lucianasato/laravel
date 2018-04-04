fd87f7ccc8fb4e4ddfdf4123332bacaa3d8c3d55
Luciana Sato <sato.lu@gmail.com>
2018-04-03 21:48:49 -0300

    Adds event to send email after registered new user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Mail\UserRegistered;
+use App\User;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::created(function($user) {
+            \Mail::to($user)->send(new UserRegistered($user));
+        });
     }
 
     /**
